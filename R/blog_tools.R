38829fdb3b8d0c40cdfdbfcd841e347ebefc5236
Mark Druffel <mark.druffel@gmail.com>
2021-01-19 15:46:03 -0800

    add theme_blog_facet

--- R/blog_tools.R
+++ R/blog_tools.R
@@ -68,6 +68,75 @@ theme_blog <- function(...){
     ggplot2::theme(...)
 }
 
+#' ggplot2 themes
+#'
+#' @param ... passed to ggplot2 theme()
+#'
+#' @details Themes all tweak existing themes but accept any ggplot2 theme arguments to overwrite settings
+#'
+#'
+#' @examples
+#' \dontrun{
+#' library(ggplot2)
+#'
+#' ggplot(mtcars, aes(wt, mpg)) +
+#'     geom_point() +
+#'     labs(title = "My Plot") +
+#'     theme_blog(text = element_text(size = 20))
+#'}
+#' @import ggplot2
+#' @import extrafont
+#' @export
+
+theme_blog_facet <- function(...){
+  options(warn = - 1, message = -1, echo = F)
+  extrafont::loadfonts()
+  ggplot2::theme_light() +
+    ggplot2::theme(
+      text = ggplot2::element_text(
+        family = "Roboto",
+        color = "#33334D",
+        size = 15,
+        inherit.blank = FALSE),
+      title = ggplot2::element_text(
+        family = "Roboto Black",
+        color = "#000000",
+        size = 18,
+        inherit.blank = FALSE),
+      axis.title = ggplot2::element_text(
+        family = "Roboto Medium",
+        color = "#33334D",
+        size = 16,
+        inherit.blank = FALSE),
+      legend.title = ggplot2::element_text(
+        family = "Roboto Medium",
+        color = "#33334D",
+        size = 16,
+        inherit.blank = FALSE),
+      legend.text = ggplot2::element_text(
+        family = "Roboto",
+        color = "#33334D",
+        size = 14,
+        hjust = 0),
+      panel.grid = ggplot2::element_blank(),
+      plot.background = ggplot2::element_rect(
+        fill = "#ffffff",
+        color = "#ffffff"),
+      plot.margin = ggplot2::margin(
+        t = 0,
+        r = 0,
+        b = 0,
+        l = 0),
+      panel.border = ggplot2::element_blank(),
+      strip.background = ggplot2::element_blank(),
+      panel.background = ggplot2::element_rect(fill = "#ffffff",
+                                               color = "#ffffff"),
+      legend.background = ggplot2::element_rect(fill = "#ffffff",
+                                                color = "#ffffff")
+    ) +
+    ggplot2::theme(...)
+}
+
 
 #' Add header using css styles
 #'
