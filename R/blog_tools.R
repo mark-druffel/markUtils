9fd9bf6ab336ec8152d5679055e56a5e399ebe08
Mark Druffel <mark.druffel@gmail.com>
2021-01-27 22:39:17 -0800

    Added network theme for ggraph

--- R/blog_tools.R
+++ R/blog_tools.R
@@ -85,7 +85,7 @@ theme_blog <- function(...){
 #' ggplot(mtcars, aes(wt, mpg)) +
 #'     geom_point() +
 #'     labs(title = "My Plot") +
-#'     theme_blog(text = element_text(size = 20))
+#'     theme_blog_facet(text = element_text(size = 20))
 #'}
 #' @import ggplot2
 #' @import extrafont
@@ -136,6 +136,69 @@ theme_blog_facet <- function(...){
     ) +
     ggplot2::theme(...)
 }
+
+
+#' ggplot2 themes
+#'
+#' @param ... passed to ggplot2 theme()
+#'
+#' @details Themes all tweak existing themes but accept any ggplot2 theme arguments to overwrite settings
+#'
+#'
+#' @examples
+#' \dontrun{
+#' library(ggplot2)
+#'
+#' ggplot(mtcars, aes(wt, mpg)) +
+#'     geom_point() +
+#'     labs(title = "My Plot") +
+#'     theme_blog_facet(text = element_text(size = 20))
+#'}
+#' @import ggplot2
+#' @import extrafont
+#' @export
+
+theme_blog_network <- function(...){
+  ggplot2::theme_void() +
+    ggplot2::theme(
+      text = ggplot2::element_text(
+        family = "Roboto Light",
+        color = "#33334D",
+        size = 11,
+        inherit.blank = FALSE),
+      title = ggplot2::element_text(
+        family = "Roboto Black",
+        color = "#000000",
+        size = 15,
+        inherit.blank = FALSE),
+      plot.subtitle = ggplot2::element_text(
+        family = "Roboto Light",
+        color = "#000000",
+        size = 12,
+        inherit.blank = FALSE),
+      axis.title = ggplot2::element_text(
+        family = "Roboto",
+        color = "#33334D",
+        size = 14,
+        inherit.blank = FALSE),
+      legend.title = ggplot2::element_text(
+        family = "Roboto",
+        color = "#33334D",
+        size = 13,
+        inherit.blank = FALSE),
+      legend.text = ggplot2::element_text(
+        family = "Roboto",
+        color = "#33334D",
+        size = 12,
+        hjust = 0),
+      plot.background = ggplot2::element_rect(
+        fill = "#ffffff",
+        color = "#ffffff")
+    ) +
+    ggplot2::theme(...)
+}
+
+
 
 
 #' Add header using css styles
